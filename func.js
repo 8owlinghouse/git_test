bfe0079990b292631adcc5e8ab7d7c5e78edcf7e
8owling <phanumas@innovisor.co.th>
2017-05-12 11:09:36 +0700

    1011

--- func.js
+++ func.js
@@ -3,12 +3,20 @@ updateStyle(docked, width, height) {
         styles.container = { ...styles.container, top: '0px' };
         styles.tabs = { ...styles.tabs, 'paddingLeft': 0, width: width - 255 };
         styles.Scrollbars = { ...styles.Scrollbars, height: height - 110 };
-        styles.divList = { ...styles.divList, width: '50%' };
+        styles.divList = { ...styles.divList, width: '60%' };
 
     } else {
         styles.container = { ...styles.container, top: '56px' };
         styles.tabs = { ...styles.tabs, 'paddingLeft': 0, width: '100%' };
         styles.Scrollbars = { ...styles.Scrollbars, height: height - 160 };
         styles.divList = { ...styles.divList, width: '100%' };
+
+        let a = 0;
+        const PI = 3.14;
     }
+}
+
+FuncA(){
+
+    console.log("User A");
 }
