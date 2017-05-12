56efd49bf3d6141a6eaca5b7391ac46b45a451af
8owling <phanumas@innovisor.co.th>
2017-05-12 11:11:53 +0700

    1012

--- func.js
+++ func.js
@@ -1,14 +1,20 @@
+FuncB(){
+
+    Console.log("I am Fnc B");
+}
 updateStyle(docked, width, height) {
     if (docked) {
+        let i = '';
+        console.log("Thhis a B");
         styles.container = { ...styles.container, top: '0px' };
         styles.tabs = { ...styles.tabs, 'paddingLeft': 0, width: width - 255 };
         styles.Scrollbars = { ...styles.Scrollbars, height: height - 110 };
-        styles.divList = { ...styles.divList, width: '50%' };
+        styles.divList = { ...styles.divList, width: '80%' };
 
     } else {
         styles.container = { ...styles.container, top: '56px' };
         styles.tabs = { ...styles.tabs, 'paddingLeft': 0, width: '100%' };
         styles.Scrollbars = { ...styles.Scrollbars, height: height - 160 };
-        styles.divList = { ...styles.divList, width: '100%' };
+        styles.divList = { ...styles.divList, width: '60%' };
     }
 }
